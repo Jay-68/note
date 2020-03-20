bce82d28c696c2930732b46a134c2a175badb4ff
jay-68 <ngari.james.n@gmail.com>
2020-03-20 10:09:06 +0300

    add confirm delete template

--- notes_app/forms.py
+++ notes_app/forms.py
@@ -1,11 +1,6 @@
 from django import forms
 from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
-# class NoteForm(forms.Form):
-#     note_title = forms.CharField(max_length=20)
-#     note_text = forms.CharField(max_length=300,widget=SummernoteWidget())
-
-
 from .models import Note
 
 
